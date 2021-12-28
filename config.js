cad3dcb150eabaa795459d5ff380140f4a22a2a9
zkminer <96770970+zkminer@users.noreply.github.com>
2021-12-28 17:14:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,6 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'thế mình làm ny nhau nhaaaaa :3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/profile.php?id=100074061462300'
